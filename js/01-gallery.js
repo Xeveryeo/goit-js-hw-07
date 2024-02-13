8c5d1f73c31adf5d4ccaab4adcb9f05fe21e1e9e
Xeveryeo <143580217+Xeveryeo@users.noreply.github.com>
2024-02-13 20:00:20 +0800

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function renderGalleryItems() {
+  galleryItems.forEach(item => {
+    const li = document.createElement('li');
+    li.classList.add('gallery__item');
+
+    const a = document.createElement('a');
+    a.classList.add('gallery__link');
+    a.href = item.original;
+
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.src = item.preview;
+    img.alt = item.description;
+    img.dataset.source = item.original;
+
+    a.appendChild(img);
+    li.appendChild(a);
+    gallery.appendChild(li);
+  });
+}
+
+renderGalleryItems();
+
+
+gallery.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const largeImageUrl = event.target.dataset.source;
+    openModal(largeImageUrl);
+  }
+}
+
+
+function openModal(imageUrl) {
+  const modal = basicLightbox.create(`
+    <img src="${imageUrl}" width="800" height="600">
+  `);
+
+  modal.show();
+}
